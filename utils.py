d85b05255bb3daac967b753360ee55b907137618
Wonjun Ko <wjko@korea.ac.kr>
2022-02-06 10:52:36 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 from sklearn.metrics import confusion_matrix, roc_auc_score
 
+# Define backpropagation
 def gradient(model, inputs, labels, rl):
     with tf.GradientTape() as tape:
         y_hat = model(inputs, rl=rl)
